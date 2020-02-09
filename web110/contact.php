9487b5dd7ab47fc3de85f938f0203dbda91e68fc
Lefty0602 <57467011+Lefty0602@users.noreply.github.com>
2020-02-09 12:26:08 -0800

    Add files via upload

--- web110/contact.php
+++ web110/contact.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width" />
   <link rel="stylesheet" href="css/portal.css" />
   <link rel="stylesheet" href="css/nav.css" />
+  <link rel="stylesheet" href="css/forms.css" />
  </head>
  <body>
      <header>
@@ -28,9 +29,23 @@
    <main class="wrapper">
        <h2 class="subheader">Contact Pedro</h2>
        <?php
-       include 'includes/simple.php'; 
-       ?>
-       <p class="clear-recaptha"></p>
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "pedro.hernandez@seattlecolleges.edu";  //place your/your client's email address here
+        $toName = "AJ Vawter and Fred Crone"; //place your client's name here
+        $website = "bbbakery.biz";  //place NAME of your client's website
+
+        //echo loadContact('simple.php');#demonstrates a simple contact form
+        echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	   ?>
 
      <footer>
       <p><small>&copy; 2020 by <a href="contact.php">Contact Pedro Hernandez </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
